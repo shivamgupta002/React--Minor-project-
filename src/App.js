3ebd6c4ed40d1895e5ccff4cb79415cbba4a485f
shivamgupta002 <shivamgupta992909@gmail.com>
2024-03-10 14:17:35 +0530

    rearrange folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Provider } from "react-redux";
 import "./App.css";
-import Count from "./count/Count";
-import store from "./count/CountStore";
+import Count from "./components/count/Count";
+import store from "./components/count/CountStore";
 
 function App() {
   return (
     <>
+      <h2>Project 1- Increment and decrement</h2>
       <Provider store={store}>
         <Count />
       </Provider>
+      
+      <h2>Project 2- Increment and decrement</h2>
     </>
   );
 }
